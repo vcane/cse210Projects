b7080d3da8c9f10857f7d2d30af8dda059af9aad
vcane <vcane6@gmail.com>
2023-04-17 20:58:02 -0500

    push prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,12 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.Write("What is your first name? ");
+    string firstName = Console.ReadLine();
+    Console.Write("What is your last name? ");
+    string lastName = Console.ReadLine();
+    Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+  }
 }
