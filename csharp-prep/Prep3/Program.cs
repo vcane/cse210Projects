eccb6d7feac4589380f18e3e1f1860b7b4c29ccc
vcane <vcane6@gmail.com>
2023-04-20 22:13:38 -0500

    my first push for prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.Write("Please enter the magic number as an integer 1-10: ");
+    string magic = Console.ReadLine();
+    int magicNum = int.Parse(magic);
+    int guessNum;
+
+    do
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+      Console.Write("Please enter an integer 1-10: ");
+      string guess = Console.ReadLine();
+      guessNum = int.Parse(guess);
+
+      if (magicNum < guessNum)
+      {
+        Console.WriteLine("Your guess is too high!");
+      }
+      else if (magicNum > guessNum)
+      {
+        Console.WriteLine("Your guess is too low!");
+      }
+      else
+      {
+        Console.WriteLine($"Congratulations, {guessNum} was the correct number!");
+      }
+    } while (guessNum != magicNum);
+  }
 }
