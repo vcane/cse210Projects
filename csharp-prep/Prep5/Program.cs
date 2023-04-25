1b9548a828e55251d482937ffd9c6beba7237d1b
vcane <vcane6@gmail.com>
2023-04-24 22:35:34 -0500

    2nd prep5 push

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+
+    static string PromptUserName()
     {
-        Console.WriteLine("Hello Prep5 World!");
+      Console.Write("Please enter your name: ");
+      string name = Console.ReadLine();
+      return name;
     }
+
+    static void DisplayWelcome()
+    {
+      Console.WriteLine("Welcome to the Program!");
+    }
+
+    static int PromptUserNumber()
+    {
+      Console.Write("Please enter your favorite number: ");
+      int number = int.Parse(Console.ReadLine());
+      return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+      int answer = number * number;
+      return answer;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+      Console.WriteLine($"{name}, the square of your number is {number}.");
+    }
+
+    DisplayWelcome();
+    string userName = PromptUserName();
+    int number = PromptUserNumber();
+    DisplayResult(userName, SquareNumber(number));
+  }
 }
